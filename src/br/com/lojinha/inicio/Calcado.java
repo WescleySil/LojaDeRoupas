e4f38117cee295c76aa28525c2005799d6012e02
Nathan <83990942+Nhzin404@users.noreply.github.com>
2023-11-29 20:34:22 -0300

    Adicionando menu calçado

---
+++ src/br/com/lojinha/inicio/Calcado.java
@@ -0,0 +1,41 @@
+package br.com.lojinha.inicio;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Nathan Monteiro Silva
+ */
+public class Calcado {
+    public void MostrarCalcado(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem vindo a seção de calçados, qual calçado deseja comprar?(Digite o número)");
+        String escolha = "0";
+        System.out.println("1. Tenis esportivo\n2.Tenis Social\n3.Tenis casual\n4.Sandalias\n5.Voltar");
+        escolha = scanner.nextLine();
+        HandleCalcado(escolha);
+
+    }
+
+    private void HandleCalcado(String escolha) {
+        ArrayList<String> menu = new ArrayList<>();
+        menu.add("1");
+        menu.add("2");
+        menu.add("3");
+        menu.add("4");
+        menu.add("5");
+        switch (escolha) {
+            default:
+                if (!escolha.equals(menu)) {
+                    System.out.println("!!Digite apenas números disponíveis no menu!!!");
+                }
+                MostrarCalcado();
+                break;
+            case "1":
+                //Calcados  calcados = new Calcados();
+            case "5":
+                Menu voltar = new Menu();
+        }
+    }
+
+}

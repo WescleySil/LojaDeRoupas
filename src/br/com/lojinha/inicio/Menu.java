901adc5cca3f451767db45ffc44551e1ffd47a5b
WescleySil <wescley.silva.cas@gmail.com>
2023-11-29 19:48:13 -0300

    Alterações menu

--- src/br/com/lojinha/inicio/Menu.java
+++ src/br/com/lojinha/inicio/Menu.java
@@ -1,9 +1,28 @@
 package br.com.lojinha.inicio;
 
+import java.util.Scanner;
+
+/**
+ *
+ *
+ * @author Wescley Silva
+ */
 public class Menu {
     public void MostrarMenu(){
-        System.out.println("Bem vindo a Lojinha, para qual seção deseja ir?");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem vindo a Lojinha, para qual seção deseja ir?(Digite o número)");
         int escolha = 0;
         System.out.println("1. Calçados\n2.Acessórios\n3.Roupas Masculino\n4.Roupas Feminino\n5.Sair");
+        escolha = scanner.nextInt();
+        HandleMenu(escolha);
+    }
+
+    public void HandleMenu(int escolha){
+        switch (escolha){
+            default:
+                System.out.println("Digite um número disponível no menu");
+            case 5:
+                System.out.println("Obrigado pela visita, volte sempre!");
+        }
     }
 }

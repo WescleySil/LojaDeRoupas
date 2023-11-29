4dbd8633c95d7aae00a1fefb80ed39e046870e8c
3dd1 <duduusoares32@gmail.com>
2023-11-29 19:38:46 -0300

    alterações classe Main

--- src/br/com/lojinha/inicio/Main.java
+++ src/br/com/lojinha/inicio/Main.java
@@ -1,8 +1,11 @@
+package br.com.lojinha.inicio;
+
 /**
  * @author Carlos Eduardo Soares
  */
 public class Main {
     public static void main(String[] args) {
-
+        Menu menu = new Menu();
+        menu.MostrarMenu();
     }
 }

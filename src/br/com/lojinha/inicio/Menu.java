3315431a3779493a6923b52f75fd1ef5482ab024
WescleySil <wescley.silva.cas@gmail.com>
2023-11-29 20:04:12 -0300

    Alterações menu

--- src/br/com/lojinha/inicio/Menu.java
+++ src/br/com/lojinha/inicio/Menu.java
@@ -1,5 +1,6 @@
 package br.com.lojinha.inicio;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -11,18 +12,30 @@ public class Menu {
     public void MostrarMenu(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bem vindo a Lojinha, para qual seção deseja ir?(Digite o número)");
-        int escolha = 0;
+        String escolha = "0";
         System.out.println("1. Calçados\n2.Acessórios\n3.Roupas Masculino\n4.Roupas Feminino\n5.Sair");
-        escolha = scanner.nextInt();
+        escolha = scanner.nextLine();
         HandleMenu(escolha);
     }
 
-    public void HandleMenu(int escolha){
+    public void HandleMenu(String escolha){
+        ArrayList<String> menu = new ArrayList<>();
+        menu.add("1");
+        menu.add("2");
+        menu.add("3");
+        menu.add("4");
+        menu.add("5");
         switch (escolha){
             default:
-                System.out.println("Digite um número disponível no menu");
-            case 5:
-                System.out.println("Obrigado pela visita, volte sempre!");
+                if(!escolha.equals(menu)){
+                    System.out.println("!!Digite apenas números disponíveis no menu!!!");
+                }
+                MostrarMenu();
+                break;
+            case "1":
+                //Calcados  calcados = new Calcados();
+            case "5":
+                break;
         }
     }
 }

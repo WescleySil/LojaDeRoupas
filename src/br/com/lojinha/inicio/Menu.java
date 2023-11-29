a8bb9c1a2b444b5d45a0096cf3437141aaf8bd5b
WescleySil <wescley.silva.cas@gmail.com>
2023-11-29 18:28:58 -0300

    Adicionar menu

---
+++ src/br/com/lojinha/inicio/Menu.java
@@ -0,0 +1,9 @@
+package br.com.lojinha.inicio;
+
+public class Menu {
+    public void MostrarMenu(){
+        System.out.println("Bem vindo a Lojinha, para qual seção deseja ir?");
+        int escolha = 0;
+        System.out.println("1. Calçados\n2.Acessórios\n3.Roupas Masculino\n4.Roupas Feminino\n5.Sair");
+    }
+}

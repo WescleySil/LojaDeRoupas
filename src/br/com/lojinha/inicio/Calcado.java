4b71cdc5706acbdde513f9c0d8e02c69e07881d9
Nathan <83990942+Nhzin404@users.noreply.github.com>
2023-11-29 21:00:47 -0300

    Alterando menu calçados

--- src/br/com/lojinha/inicio/Calcado.java
+++ src/br/com/lojinha/inicio/Calcado.java
@@ -11,7 +11,7 @@ public class Calcado {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bem vindo a seção de calçados, qual calçado deseja comprar?(Digite o número)");
         String escolha = "0";
-        System.out.println("1. Tênis esportivo\n2.Tênis Social\n3.Tênis casual\n4.Sandalias\n5.Voltar");
+        System.out.println("1.Tênis esportivo\n2.Tênis Social\n3.Tênis casual\n4.Sandalias\n5.Voltar");
         escolha = scanner.nextLine();
         HandleCalcado(escolha);
 
@@ -24,6 +24,7 @@ public class Calcado {
         menu.add("3");
         menu.add("4");
         menu.add("5");
+        Scanner scanner = new Scanner(System.in);
         switch (escolha) {
             default:
                 if (!escolha.equals(menu)) {
@@ -32,7 +33,37 @@ public class Calcado {
                 MostrarCalcado();
                 break;
             case "1":
-                //Calcados  calcados = new Calcados();
+                System.out.println("Tênis esportivo comprado com sucesso!!");
+                System.out.println("Deseja comprar outro sapato?\n1.sim 2.não (Digite um número)");
+                if (!scanner.nextLine().equals("1")){
+                    new Menu().MostrarMenu();
+                }
+                MostrarCalcado();
+                break;
+            case "2":
+                System.out.println("Tênis Social comprado com sucesso!!");
+                System.out.println("Deseja comprar outro sapato?\n1.sim 2.não (Digite um número)");
+                if (!scanner.nextLine().equals("1")){
+                    new Menu().MostrarMenu();
+                }
+                MostrarCalcado();
+                break;
+            case "3":
+                System.out.println("Tênis casual comprado com sucesso!!");
+                System.out.println("Deseja comprar outro sapato?\n1.sim 2.não (Digite um número)");
+                if (!scanner.nextLine().equals("1")){
+                    new Menu().MostrarMenu();
+                }
+                MostrarCalcado();
+                break;
+            case "4":
+                System.out.println("Sandalia comprada com sucesso!!");
+                System.out.println("Deseja comprar outra sandalia?\n1.sim 2.não (Digite um número)");
+                if (!scanner.nextLine().equals("1")){
+                    new Menu().MostrarMenu();
+                }
+                MostrarCalcado();
+                break;
             case "5":
                 Menu voltar = new Menu();
                 voltar.MostrarMenu();
